bdd96f723463823e0580027b260f27e5376c1014
siyan liu <siyanliuads@gmail.com>
2024-07-12 19:39:36 -0400

    manual address input logic

---
+++ src/popup/popup.js
@@ -0,0 +1,27 @@
+document.getElementById('addressForm').addEventListener('submit', (e) => {
+    e.preventDefault();
+    const address = document.getElementById('addressInput').value;
+    chrome.storage.sync.get({addresses: []}, (data) => {
+      let addresses = data.addresses;
+      addresses.push(address);
+      chrome.storage.sync.set({addresses: addresses}, () => {
+        document.getElementById('addressInput').value = '';
+        loadAddresses();
+      });
+    });
+  });
+  
+  function loadAddresses() {
+    chrome.storage.sync.get({addresses: []}, (data) => {
+      const addressList = document.getElementById('addressList');
+      addressList.innerHTML = '';
+      data.addresses.forEach((address, index) => {
+        const div = document.createElement('div');
+        div.textContent = address;
+        addressList.appendChild(div);
+      });
+    });
+  }
+  
+  loadAddresses();
+  
