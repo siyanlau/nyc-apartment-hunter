22922d9b247eadc8b09e0748a1af6c71f051d113
siyan liu <siyanliuads@gmail.com>
2024-07-16 03:04:21 -0400

    changed await/async to promise chaining to improve readability

--- src/api/fetchComplaintData.js
+++ src/api/fetchComplaintData.js
@@ -1,20 +1,40 @@
 const endpoint = 'https://data.cityofnewyork.us/resource/jrb2-thup.json?incident_address=';
 
-async function fetchComplaintData(address) {
-    try {
-      const response = await fetch(endpoint + encodeURIComponent(address));
-      if (!response.ok) {
-        console.log("NYC open data response was not ok");
-      }
-      else {
-        console.log("NYC open data connection was ok");
-      }
-      const data = await response.json();
-      const simplifiedData = data.map(complaint => ({ descriptor: complaint.descriptor }));
-      return simplifiedData;
-      // need to do more parsing / cleaning here
-    } catch (error) {
-      console.error('There was a problem with the fetch operation:', error);
-      return null;
-    }
-  }
+// async function fetchComplaintData(address) {
+//     try {
+//       const response = await fetch(endpoint + encodeURIComponent(address));
+//       if (!response.ok) {
+//         console.log("NYC open data response was not ok");
+//       }
+//       else {
+//         console.log("NYC open data connection was ok");
+//       }
+//       const data = await response.json();
+//       const simplifiedData = data.map(complaint => ({ descriptor: complaint.descriptor }));
+//       return simplifiedData;
+//       // need to do more parsing / cleaning here
+//     } catch (error) {
+//       console.error('There was a problem with fetching from NYC open data: ', error);
+//       return null;
+//     }
+//   }
+
+function fetchComplaintData(address) {
+    return fetch(endpoint + encodeURIComponent(address))
+        .then(response => {
+            if (!response.ok) {
+                console.log("NYC open data response was not ok");
+            } else {
+                console.log("NYC open data connection was ok");
+            }
+            return response.json();
+        })
+        .then(data => {
+            const simplifiedData = data.map(complaint => ({ descriptor: complaint.descriptor }));
+            return simplifiedData;
+        })
+        .catch(error => {
+            console.error('There was a problem with fetching from NYC open data: ', error);
+            return null;
+        });
+}
