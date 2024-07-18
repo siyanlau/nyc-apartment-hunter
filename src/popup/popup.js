e3c85e498c692cdb94df20a6561882fa8f1bc905
siyan liu <siyanliuads@gmail.com>
2024-07-17 23:06:22 -0400

    finished all basic plumbing

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -12,6 +12,7 @@ addressForm.addEventListener('submit', (e) => {
       if (response) {
         document.getElementById('addressInput').value = ''; // clear the input field
         console.log("8. Message sent successfully and response received: ", response);
+        loadAddresses();
       } else {
         console.log("No response received", response);
       }
@@ -24,17 +25,27 @@ addressForm.addEventListener('submit', (e) => {
 
 function loadAddresses() {
   chrome.storage.sync.get({ addresses: [] }, (data) => {
-    console.log("addresses stored in background: ", data.addresses);
-  })
+    console.log("9. addresses stored in background: ", data.addresses);
+    const addresses = data.addresses;
+    const addressList = document.getElementById('addressList');
+    addressList.innerHTML = '';
+    addresses.forEach((item) => {
+      const li = document.createElement('li');
+      li.textContent = item.address;
+      const complaintsDiv = document.createElement('div');
+      complaintsDiv.classList.add('complaints');
+      complaintsDiv.innerHTML = `<h3>Complaints for ${item.address}</h3>`;
+      item.complaints.forEach(complaint => {
+        const complaintItem = document.createElement('p');
+        complaintItem.textContent = complaint.descriptor;
+        complaintsDiv.appendChild(complaintItem);
+      });
+      li.appendChild(complaintsDiv);
+      addressList.appendChild(li);
+    });
+  });
 }
 
-
-//   chrome.runtime.sendMessage({ action: 'getAddresses' }, (addresses) => { // don't want to load addresses every time, needs refactor
-//     if (chrome.runtime.lastError) {
-//       console.error(chrome.runtime.lastError.message);
-//       alert('Failed to load addresses.');
-//       return;
-//     }
 
 //     const addressList = document.getElementById('addressList');
 //     addressList.innerHTML = '';
