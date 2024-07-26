0e38ef5ec8c8cfa5e3585b6106178fd848ef4a46
siyan liu <siyanliuads@gmail.com>
2024-07-26 02:35:07 -0400

    geocode + address formating + address parsing successful

--- src/api/geocode.js
+++ src/api/geocode.js
@@ -2,18 +2,22 @@ import CONFIG from "../config.js"
 
 const apiKey = CONFIG.API_KEY;
 
-export const geocode = (address) => {
+export const geocode = async (address) => {
     const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`;
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-            if (data.status === 'OK') {
-                const location = data.results[0].geometry.location;
-                console.log(`Geocode successful, Latitude: ${location.lat}, Longitude: ${location.lng}`);
-            } else {
-                console.error('Geocoding error:', data.status);
-            }
-        })
-        .catch(error => console.error('Error:', error));
+    
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+        if (data.status === 'OK') {
+            console.log(`Geocode successful`);
+            return data;
+        } else {
+            console.error('Geocoding error:', data.status);
+            return null;
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        return null;
+    }
 }
