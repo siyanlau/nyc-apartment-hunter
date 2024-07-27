304740310f553636f2c341c130eac2e3a81b558e
siyan liu <siyanliuads@gmail.com>
2024-07-27 01:25:11 -0400

    commute departure time set to 10:00 am the next day

--- src/popup/test.js
+++ src/popup/test.js
@@ -1,5 +1,10 @@
-import { walkingDurantion } from "../utils/api/commuteDuration.js";
+import { commuteDurantion } from "../utils/api/commuteDuration.js";
+import { geocode } from "../utils/api/geocode.js";
 
 const dest_id = "ChIJq7z38-FEwokReJgESNhW2T8", start_id = "EioxNTU5IFcgNnRoIFN0ICMyZCwgQnJvb2tseW4sIE5ZIDExMjA0LCBVU0EiHhocChYKFAoSCav7OK7jRMKJEeFpQNyCViQrEgIyZA";
-const walk_to_subway_data = await walkingDurantion(start_id, dest_id);
+const walk_to_subway_data = await commuteDurantion(start_id, dest_id, "walking");
 console.log("walking duration to subway: ", walk_to_subway_data);
+
+const home_id = "ChIJj2jUHLRawokRP4W43n8R5JA";
+const transit_duration_data = await commuteDurantion(start_id, home_id, "transit");
+console.log("transit duration ", transit_duration_data);
--- src/utils/api/commuteDuration.js
+++ src/utils/api/commuteDuration.js
@@ -2,8 +2,9 @@ import CONFIG from "../../config.js"
 
 const apiKey = CONFIG.API_KEY;
 
-export const walkingDurantion = (start_id, dest_id) => {
-    const url = `https://maps.googleapis.com/maps/api/directions/json?destination=place_id:${dest_id}&origin=place_id:${start_id}&key=${apiKey}&mode=walking`;
+export const commuteDurantion = (start_id, dest_id, method) => {
+    const departureTimestamp = getFutureTimestamp(10, 0);
+    const url = `https://maps.googleapis.com/maps/api/directions/json?destination=place_id:${dest_id}&origin=place_id:${start_id}&key=${apiKey}&mode=${method}&departure_time=${departureTimestamp}`;
 
     return fetch(url)
         .then(response => {
@@ -14,35 +15,25 @@ export const walkingDurantion = (start_id, dest_id) => {
         })
         .then(data => {
             if (data.status !== 'OK') {
-                throw new Error('Error fetching walking directions: ' + data.status);
+                throw new Error(`Error fetching ${method} directions: ` + data.status);
             }
+            console.log(data);
             const leg = data.routes[0].legs[0];
             return leg.duration;
         })
         .catch(error => {
-            console.error('Error fetching walking distance:', error);
+            console.error(`Error fetching ${method} distance:`, error);
             throw error;
         });
 }
 
-export const transitDuration = (start_id, dest_id) => {
-    const url = `https://maps.googleapis.com/maps/api/directions/json
-      ?destination=place_id%${dest_id}
-      &origin=place_id%${start_id}
-      &key=${apiKey}
-      &mode=transit`
-
-    return fetch(url)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            console.log("transit duration response: ", response);
-            return response.json();
-        })
-        .then(data => data.result)
-        .catch(error => {
-            console.error('Error fetching walking distance:', error);
-            throw error;
-        })
-}
+function getFutureTimestamp(hour, minute) {
+    // Get the current date and time
+    const now = new Date();
+  
+    // Set the date to the next day at the specified hour and minute
+    const futureTime = new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1, hour, minute, 0);
+  
+    // Convert the date to a Unix timestamp (seconds since Jan 1, 1970)
+    return Math.floor(futureTime.getTime() / 1000);
+  }
