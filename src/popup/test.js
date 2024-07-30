f59f47d052826cbce5945ee0a7300fbbd15558d4
siyan liu <siyanliuads@gmail.com>
2024-07-29 23:03:55 -0400

    ethnicity percentage calculation done

--- src/popup/test.js
+++ src/popup/test.js
@@ -8,4 +8,21 @@ import { getDecennial } from "../utils/api/getDecennial.js";
 const blockGroupGEOID = '360470432001'; // Example GEOID for a block GROUP!! Note this is GROUP level GEOID
 const apiKey = CONFIG.CENSUS_KEY;
 
-getDecennial(blockGroupGEOID, apiKey).then(data => console.log(data));
+getDecennial(blockGroupGEOID, apiKey).then(data => console.log(ethnicityComposition(data)));
+
+const ethnicityComposition = (data) => {
+    const total = parseInt(data[1][0]);
+
+    // Convert each count to a percentage and format it
+    const whitePercentage = ((parseInt(data[1][1]) / total) * 100).toFixed(1);
+    const blackPercentage = ((parseInt(data[1][2]) / total) * 100).toFixed(1);
+    const asianPercentage = ((parseInt(data[1][3]) / total) * 100).toFixed(1);
+    const hispanicPercentage = ((parseInt(data[1][4]) / total) * 100).toFixed(1);
+
+    return {
+        whitePercentage: `${whitePercentage}%`, 
+        blackPercentage: `${blackPercentage}%`, 
+        asianPercentage: `${asianPercentage}%`, 
+        hispanicPercentage: `${hispanicPercentage}%`
+    };
+}
