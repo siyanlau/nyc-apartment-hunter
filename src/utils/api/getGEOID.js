8f54088c4de369f80b2505f0dc6135235a7532cc
siyan liu <siyanliuads@gmail.com>
2024-07-29 21:37:25 -0400

    getGEOID passed test

--- src/utils/api/getGEOID.js
+++ src/utils/api/getGEOID.js
@@ -1,5 +1,42 @@
-export const getGEOID = (formattedAddress) => {
+export const getGEOID = async (formattedAddress) => {
+    const {street, city, state, zip} = addressComponentExtractor(formattedAddress);
+    if (!street || !city || !state || !zip) {
+        console.log("something went wrong with address component extraction");
+        return null;
+    }
+    const baseUrl = "https://geocoding.geo.census.gov/geocoder/geographies/address";
+    const params = new URLSearchParams({
+        street: street,
+        city: city,
+        state: state,
+        zip: zip,
+        benchmark: "Public_AR_Current",
+        vintage: "Current_Current",
+        format: "json",
+        layers: "10,12"
+    });
 
+    const url = `${baseUrl}?${params.toString()}`;
+    console.log("url:", url);
+
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if (data.result.addressMatches.length > 0) {
+            const geographies = data.result.addressMatches[0].geographies;
+
+            // Get GEOID for Census Block
+            const blockGEOID = geographies['2020 Census Blocks'][0].GEOID;
+            const blockGroupGEOID = geographies['Census Block Groups'][0].GEOID;
+
+            // You can similarly retrieve the GEOID for Census Tract if it's available in the response
+            // const tractGEOID = geographies['Census Tracts'] ? geographies['Census Tracts'][0].GEOID : 'N/A';
+            return {blockGEOID: blockGEOID, blockGroupGEOID: blockGroupGEOID};
+        }
+    } catch (error) {
+        console.error('Error fetching geocoder data:', error);
+    }
 }
 
 const addressComponentExtractor = (formattedAddress) => {
